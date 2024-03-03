c78b00ba0d7fd831bec8b6ae93cdefee7083c04a
Miguel <miguelmvilela12@gmail.com>
2024-03-03 11:15:06 -0300

    Adcionando paginação ao projeto

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render,get_object_or_404,redirect
-# from django.http import Http404
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[10:20]
+        .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -35,10 +39,15 @@ def search(request):
             Q(email__icontains=search_value)
         )\
         .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
-        'site_title': 'Search - '
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
@@ -50,11 +59,6 @@ def search(request):
 
 
 def contact(request,contact_id):
-    # single_contact = Contact.objects.filter(pk= contact_id).first()
-
-    # if single_contact is None:
-    #     raise Http404()
-
     single_contact = get_object_or_404(Contact,pk= contact_id, show = True)
 
     site_title = f'{single_contact.first_name} {single_contact.last_name} - '
